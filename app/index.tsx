52336b1f956983a78ef9100fb18d3ce4ad4ba95f
Eduardo <eduardocaneppele74@gmail.com>
2025-12-31 13:27:18 -0300

    feat: import vector icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,17 @@
 import { Text, View, StyleSheet } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 export default function Index() {
-
   type InfoBoxProps = {
     label: string;
     value: string;
     color?: string;
+  };
+
+  type HeaderProps = {
+    profileimg: string;
+    month: string;
   };
 
   const InfoBox = ({ label, value, color }: InfoBoxProps) => (
@@ -16,18 +21,31 @@ export default function Index() {
     </View>
   );
 
+  const Header = ({ profileimg, month }: HeaderProps) => (
+    <View style={[styles.header, styles.row]}>
+      <Text style={{ textAlign: "center" }}>
+        <Icon name="person" size={30} />
+      </Text>
+      <Text style={[{ textAlign: "center"}, styles.title]}>
+        {month}
+        <Icon name="arrow-drop-down" size={25}/>
+      </Text>
+      <Text style={{ textAlign: "center" }}>
+        <Icon name="menu" size={30} />
+      </Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <View style={[styles.header, styles.row]}>
-        <Text style={styles.profileimg}>Imagem aqui</Text>
-      </View>
+      <Header profileimg="Img" month="Janeiro" />
       <View style={[styles.content, styles.row]}>
         <View style={styles.contentbox}>
           <Text style={styles.text}>Saldo total</Text>
           <Text style={styles.title}>R$ 8.900,87</Text>
           <View style={[styles.contentboxinfo, styles.row]}>
             <InfoBox label="Receitas" value="R$ 4.500" color="green" />
-            <InfoBox label="Despesas" value="R$ -4.500" color="red"/>
+            <InfoBox label="Despesas" value="R$ -4.500" color="red" />
           </View>
         </View>
       </View>
@@ -35,8 +53,8 @@ export default function Index() {
   );
 }
 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
   row: {
     flexDirection: "row"
   },
@@ -49,14 +67,9 @@ const styles = StyleSheet.create({
   header: {
     flex: 0.17,
     backgroundColor: "rgba(37, 97, 236, 1)",
-    alignItems: "center"
-  },
-
-  profileimg: {
-    flex: 0.1,
-    backgroundColor: "#ff0",
-    marginLeft: 20,
-    borderRadius: 50
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
   },
 
   text: {
@@ -91,7 +104,12 @@ const styles = StyleSheet.create({
     height: 148,
     top: -48,
     alignItems: "center",
-    padding: 10
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+    elevation: 10,
   },
 
   contentboxinfo: {
